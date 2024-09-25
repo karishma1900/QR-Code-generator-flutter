80b78c1a3ec24bdad6b31d709a8b66572bd78f9d
Karishma's Classes <156506602+karishma1900@users.noreply.github.com>
2024-09-25 17:12:31 +0530

    Update scan_qr_code.dart

--- lib/scan_qr_code.dart
+++ lib/scan_qr_code.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
+import 'package:simple_barcode_scanner/simple_barcode_scanner.dart';
 
 class ScanQRCode extends StatefulWidget {
   const ScanQRCode({Key? key}) : super(key: key);
@@ -12,46 +12,9 @@ class ScanQRCode extends StatefulWidget {
 
 class _ScanQRCodeState extends State<ScanQRCode> {
   String qrResult = 'Scanned Data will appear here';
-  bool isScanning = false;  // Added to track scanning status
+// Added to track scanning status
 
-  Future<void> scanQR() async {
-    setState(() {
-      isScanning = true;  // Set scanning status to true
-    });
-    
-    try {
-      final qrCode = await FlutterBarcodeScanner.scanBarcode(
-        '#ff6666',
-        'Cancel',
-        true,
-        ScanMode.QR,
-      );
-
-      if (!mounted) return;
-
-      if (qrCode != '-1') {  // Check if the scan was not canceled
-        setState(() {
-          this.qrResult = qrCode;
-        });
-      } else {
-        setState(() {
-          this.qrResult = 'Scan canceled';
-        });
-      }
-    } on PlatformException {
-      setState(() {
-        qrResult = 'Failed to read QR Code';
-      });
-    } catch (e) {
-      setState(() {
-        qrResult = 'Error: $e';
-      });
-    } finally {
-      setState(() {
-        isScanning = false;  // Reset scanning status
-      });
-    }
-  }
+  
 
   @override
   Widget build(BuildContext context) {
@@ -63,19 +26,23 @@ class _ScanQRCodeState extends State<ScanQRCode> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            SizedBox(height: 30),
-            if (isScanning)  // Show a loading indicator while scanning
-              CircularProgressIndicator(),
-            Text(
-              '$qrResult',
-              style: TextStyle(color: Colors.black),
-              textAlign: TextAlign.center,
-            ),
-            SizedBox(height: 30),
-            ElevatedButton(
-              onPressed: isScanning ? null : scanQR,  // Disable button while scanning
-              child: Text('Scan Code'),
-            ),
+        ElevatedButton(
+              onPressed: () async {
+                var res = await Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const SimpleBarcodeScannerPage(),
+                    ));
+                setState(() {
+                  if (res is String) {
+                    qrResult = res;
+                  }
+                });
+              },
+              child: const Text('Open Scanner'),
+            )
+           
+            
           ],
         ),
       ),
