c0662eca6d035edcb429601a413d6b77899e7f0e
Karishma's Classes <156506602+karishma1900@users.noreply.github.com>
2024-09-26 10:12:39 +0530

    Update scan_qr_code.dart

--- lib/scan_qr_code.dart
+++ lib/scan_qr_code.dart
@@ -1,49 +1,56 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:simple_barcode_scanner/simple_barcode_scanner.dart';
+import 'package:qr_flutter/qr_flutter.dart';
 
-class ScanQRCode extends StatefulWidget {
-  const ScanQRCode({Key? key}) : super(key: key);
+class GenerateQRCode extends StatefulWidget {
+  const GenerateQRCode({Key? key}) : super(key: key);
 
   @override
-  _ScanQRCodeState createState() => _ScanQRCodeState();
+  _GenerateQRCodeState createState() => _GenerateQRCodeState();
 }
 
-class _ScanQRCodeState extends State<ScanQRCode> {
-  String qrResult = 'Scanned Data will appear here';
-// Added to track scanning status
-
-  
+class _GenerateQRCodeState extends State<GenerateQRCode> {
+  TextEditingController urlController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('QR Code Scanner'),
+        title: Text('Generate QR Code'),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-        ElevatedButton(
-              onPressed: () async {
-                var res = await Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => const SimpleBarcodeScannerPage(),
-                    ));
-                setState(() {
-                  if (res is String) {
-                    qrResult = res;
-                  }
-                });
-              },
-              child: const Text('Open Scanner'),
-            )
-           
-            
-          ],
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              if (urlController.text.isNotEmpty)
+                QrImageView(
+                  data: urlController.text,
+                  version: QrVersions.auto,
+                  size: 200.0,
+                ),
+              SizedBox(height: 10),
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 10),
+                child: TextField(
+                  controller: urlController,
+                  decoration: InputDecoration(
+                    hintText: 'Enter Your Data',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                    labelText: 'Enter Your Data',
+                  ),
+                ),
+              ),
+              SizedBox(height: 10),
+              ElevatedButton(
+                onPressed: () {
+                  setState(() {});
+                },
+                child: Text('Generate QR Code'),
+              ),
+            ],
+          ),
         ),
       ),
     );
