996accf6e7a0e70c492343f9fca3c6e20a39e3e1
Karishma's Classes <156506602+karishma1900@users.noreply.github.com>
2024-09-26 10:26:45 +0530

    Update scan_qr_code.dart

--- lib/scan_qr_code.dart
+++ lib/scan_qr_code.dart
@@ -1,58 +1,66 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:qr_flutter/qr_flutter.dart';
 
-class GenerateQRCode extends StatefulWidget {
-  const GenerateQRCode({Key? key}) : super(key: key);
+import 'package:simple_barcode_scanner/simple_barcode_scanner.dart';
+
+class ScanQRCode extends StatefulWidget {
+  const ScanQRCode({super.key});
 
   @override
-  _GenerateQRCodeState createState() => _GenerateQRCodeState();
+  State<ScanQRCode> createState() => _ScanQRCodeState();
 }
 
-class _GenerateQRCodeState extends State<GenerateQRCode> {
-  TextEditingController urlController = TextEditingController();
+class _ScanQRCodeState extends State<ScanQRCode> {
+  String? result;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Generate QR Code'),
-      ),
+      appBar: AppBar(title: Text('QR Code Scanner')),
       body: Center(
-        child: SingleChildScrollView(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              if (urlController.text.isNotEmpty)
-                QrImageView(
-                  data: urlController.text,
-                  version: QrVersions.auto,
-                  size: 200.0,
-                ),
-              SizedBox(height: 10),
-              Container(
-                padding: EdgeInsets.symmetric(horizontal: 10),
-                child: TextField(
-                  controller: urlController,
-                  decoration: InputDecoration(
-                    hintText: 'Enter Your Data',
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(15),
-                    ),
-                    labelText: 'Enter Your Data',
-                  ),
-                ),
-              ),
-              SizedBox(height: 10),
-              ElevatedButton(
-                onPressed: () {
-                  setState(() {});
-                },
-                child: Text('Generate QR Code'),
-              ),
-            ],
-          ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () async {
+                var res = await Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const SimpleBarcodeScannerPage(),
+                    ));
+                setState(() {
+                  if (res is String) {
+                    result = res;
+                    _showResultDialog(context, result!);
+                  }
+                });
+              },
+              child: const Text('Open Scanner'),
+            ),
+            Text('Scan Result: $result'),
+          ],
         ),
       ),
+    );
+  }
+
+  void _showResultDialog(BuildContext context, String result) {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text('Scan Result'),
+          content: Text(result),
+          actions: [
+            ElevatedButton(
+              child: Text('OK'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
     );
   }
 }
