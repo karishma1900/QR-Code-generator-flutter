bd72ebcfb204eec86f7441aaba28fc03a974bf06
Karishma's Classes <156506602+karishma1900@users.noreply.github.com>
2024-09-26 10:34:23 +0530

    Update scan_qr_code.dart

--- lib/scan_qr_code.dart
+++ lib/scan_qr_code.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
 import 'package:simple_barcode_scanner/simple_barcode_scanner.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class ScanQRCode extends StatefulWidget {
   const ScanQRCode({super.key});
@@ -31,7 +31,7 @@ class _ScanQRCodeState extends State<ScanQRCode> {
                 setState(() {
                   if (res is String) {
                     result = res;
-                    _showResultDialog(context, result!);
+                    _handleScanResult(result!);
                   }
                 });
               },
@@ -42,6 +42,22 @@ class _ScanQRCodeState extends State<ScanQRCode> {
         ),
       ),
     );
+  }
+
+  void _handleScanResult(String result) {
+    if (result.startsWith('http://') || result.startsWith('https://')) {
+      _launchUrl(result);
+    } else {
+      _showResultDialog(context, result);
+    }
+  }
+
+  void _launchUrl(String url) async {
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      _showResultDialog(context, 'Could not launch $url');
+    }
   }
 
   void _showResultDialog(BuildContext context, String result) {
